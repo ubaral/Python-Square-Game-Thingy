96dcd61727d572701373abf2e4e3b6c7961d6621
Utsav Baral <utsavbaral@gmail.com>
2016-02-07 00:11:13 -0800

    unfinished ai dots and squares project, have to fix buggy implementation of alpha beta pruning

--- game.py
+++ game.py
@@ -1,51 +1,54 @@
-import sys, pygame, math
+import sys
+import pygame
+import math
 from playerLogic import Player
 
 pygame.init()
 
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-RED = (255,0,0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 BLUE = (0, 0, 255)
-LIMEGREEN = (124,252,0)
-BGCOLOR = (255,228,225)
+LIMEGREEN = (124, 252, 0)
+BGCOLOR = (255, 228, 225)
+
 
 class Dot(object):
-	DOT_RADIUS = 10
+    DOT_RADIUS = 10
 
-	def __init__(self, pos, grdPos, clr=BLACK):
-		self.radius = Dot.DOT_RADIUS
-		self.position = pos
-		self.color = dict({'r':clr[0], 'g':clr[1], 'b':clr[2]})
-		self.gridPosition = grdPos
+    def __init__(self, pos, grdPos, clr=BLACK):
+        self.radius = Dot.DOT_RADIUS
+        self.position = pos
+        self.color = dict({'r': clr[0], 'g': clr[1], 'b': clr[2]})
+        self.gridPosition = grdPos
 
-	def getClrTuple(self):
-		return (self.color['r'], self.color['g'], self.color['b'])
+    def getClrTuple(self):
+        return (self.color['r'], self.color['g'], self.color['b'])
 
-	def setClrByTuple(self, clrTup):
-		self.color['r'] = clrTup[0]
-		self.color['g'] = clrTup[1]
-		self.color['b'] = clrTup[2]
+    def setClrByTuple(self, clrTup):
+        self.color['r'] = clrTup[0]
+        self.color['g'] = clrTup[1]
+        self.color['b'] = clrTup[2]
 
-	def __str__(self):
-		return "Dot(" + str(self.gridPosition) + ")"
+    def __str__(self):
+        return "Dot(" + str(self.gridPosition) + ")"
 
-	def __repr__(self):
-		return "Dot(" + str(self.gridPosition) + ")"
+    def __repr__(self):
+        return "Dot(" + str(self.gridPosition) + ")"
 
-	def __eq__(self, other):
-		return ((self.position[0]==other.position[0]) and (self.position[1]==other.position[1]))
+    def __eq__(self, other):
+        return ((self.position[0] == other.position[0]) and (self.position[1] == other.position[1]))
 
 size = width, height = 600, 600
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('DOTS')
 
 try:
-	dotsWidth = int(sys.argv[1])
-	dotsHeight = int(sys.argv[2])
+    dotsWidth = int(sys.argv[1])
+    dotsHeight = int(sys.argv[2])
 except (IndexError, ValueError):
-	print("No/Improper input so will default to 5 x 5 grid")
-	dotsWidth = dotsHeight = 5
+    print("No/Improper input so will default to 5 x 5 grid")
+    dotsWidth = dotsHeight = 5
 
 myfont = pygame.font.Font(None, 25)
 text = myfont.render("Welcome to the Game of Squares and Shit!", 1, BLACK)
@@ -55,28 +58,44 @@ myfont = pygame.font.Font(None, 35)
 turn = myfont.render("Player Turn: P1", 1, BLACK)
 turnRect = turn.get_rect()
 
-#Stupid way to do this buut fuck it...
+# Stupid way to do this buut fuck it...
 dots = []
 dots2 = []
 
-deltaWidth = int((width - width/dotsWidth)/dotsWidth)
-deltaHeight = int(((height) - (height)/dotsHeight)/dotsHeight)
+deltaWidth = int((width - width / dotsWidth) / dotsWidth)
+deltaHeight = int(((height) - (height) / dotsHeight) / dotsHeight)
 xKnot = 0
-#create the set of dots
+# create the set of dots
 for i in range(dotsWidth):
- 	xKnot += deltaWidth
- 	yKnot = 15
- 	sublist = []
-	for j in range(dotsHeight):
- 		yKnot += deltaHeight
- 		sublist.append( Dot((xKnot,yKnot), (i,j)) )
- 	dots2.append(sublist)
- 	dots = dots + sublist
+    xKnot += deltaWidth
+    yKnot = 15
+    sublist = []
+    for j in range(dotsHeight):
+        yKnot += deltaHeight
+        sublist.append(Dot((xKnot, yKnot), (i, j)))
+    dots2.append(sublist)
+    dots = dots + sublist
 
 squares = []
 for i in range(dotsWidth - 1):
-	for j in range(dotsHeight - 1):
-		squares.append (([frozenset([dots2[i][j], dots2[i+1][j]]),frozenset([dots2[i][j], dots2[i][j+1]]),frozenset([dots2[i+1][j+1], dots2[i][j+1]]),frozenset([dots2[i+1][j+1], dots2[i+1][j]])], [dots2[i][j].position, dots2[i+1][j].position, dots2[i+1][j+1].position, dots2[i][j+1].position,dots2[i][j].position]))
+    for j in range(dotsHeight - 1):
+        squares.append(([frozenset([dots2[i][j], dots2[i + 1][j]]), frozenset([dots2[i][j], dots2[i][j + 1]]), frozenset([dots2[i + 1][j + 1], dots2[i][j + 1]]), frozenset(
+            [dots2[i + 1][j + 1], dots2[i + 1][j]])], [dots2[i][j].position, dots2[i + 1][j].position, dots2[i + 1][j + 1].position, dots2[i][j + 1].position, dots2[i][j].position]))
+
+
+possibleMoveSet = set()
+for column in range(len(dots2) - 1):
+    for dot in range(len(dots2[column]) - 1):
+        possibleMoveSet.add(frozenset([dots2[column][dot], dots2[column][dot + 1]]))
+        possibleMoveSet.add(frozenset([dots2[column][dot], dots2[column + 1][dot]]))
+
+    lastDot = dots2[column][-1]  # process last dot of the column ...
+    possibleMoveSet.add(
+        frozenset([lastDot, dots2[column + 1][len(dots2[column + 1]) - 1]]))
+
+lastColumn = dots2[-1]  # process last column of square ....
+for dot in range(len(lastColumn) - 1):
+    possibleMoveSet.add(frozenset([lastColumn[dot], lastColumn[dot + 1]]))
 
 dotclick = (False, (-1, -1))
 clickedDot = None
@@ -84,106 +103,159 @@ connectedDotsSet = set()
 rectsToDraw = set()
 
 P_1 = Player("P1", (127, 0, 255), False)
-P_2 = Player("P2", BLUE, False)
+P_2 = Player("P2", BLUE, True)
 PList = [P_1, P_2]
 turnNum, numPlayers = 0, len(PList)
-currPlayerTurn = PList[turnNum%numPlayers]
+currPlayerTurn = PList[turnNum % numPlayers]
 recColorDict = dict()
-totalSquares = dotsHeight*dotsWidth - dotsHeight - dotsWidth + 1
+totalSquares = dotsHeight * dotsWidth - dotsHeight - dotsWidth + 1
 re = False
 
+# init = [frozenset([dots2[0][0], dots2[0][1]]), frozenset([dots2[0][1], dots2[0][2]])]
+# for connection in init:
+#     connectedDotsSet.add(connection)
+#     possibleMoveSet.remove(connection)
+
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			dotclick = (True, pygame.mouse.get_pos())
-		if event.type == pygame.MOUSEBUTTONUP:
-			dotclick = (False, pygame.mouse.get_pos(), clickedDot)
-			clickedDot = None
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            dotclick = (True, pygame.mouse.get_pos())
+        if event.type == pygame.MOUSEBUTTONUP:
+            dotclick = (False, pygame.mouse.get_pos(), clickedDot)
+            clickedDot = None
 
-	screen.fill(BGCOLOR)
-	screen.blit(text, (width/2 - textRect.width/2, 10))
-	turn = myfont.render("Player Turn: "+str(currPlayerTurn.playerName), 1, currPlayerTurn.color)
-	screen.blit(turn, (width/2 - turnRect.width/2, 40))
+    screen.fill(BGCOLOR)
+    screen.blit(text, (width / 2 - textRect.width / 2, 10))
+    turn = myfont.render(
+        "Player Turn: " + str(currPlayerTurn.playerName), 1, currPlayerTurn.color)
+    screen.blit(turn, (width / 2 - turnRect.width / 2, 40))
 
-	for rect in rectsToDraw:
-		pygame.draw.polygon(screen, recColorDict[rect], list(rect), 0)
+    for rect in rectsToDraw:
+        pygame.draw.polygon(screen, recColorDict[rect], list(rect), 0)
 
-	for frozenDotSet in connectedDotsSet:
-		dot1, dot2 = frozenDotSet
-		pygame.draw.line(screen, BLACK, dot1.position, dot2.position, 10)
+    for frozenDotSet in connectedDotsSet:
+        dot1, dot2 = frozenDotSet
+        pygame.draw.line(screen, BLACK, dot1.position, dot2.position, 10)
 
-	for dot in dots:
-		if currPlayerTurn.isAI == False:
-			distance = math.sqrt((pygame.mouse.get_pos()[0] - dot.position[0])**2 + (pygame.mouse.get_pos()[1] - dot.position[1])**2)
-			if dotclick[1][0] > 0:
-				clickDist = math.sqrt((dotclick[1][0] - dot.position[0])**2 + (dotclick[1][1]-dot.position[1])**2)
-			else:
-				clickDist = float("inf")
-			
-			if distance > Dot.DOT_RADIUS:
-				for key in dot.color:
-					if dot.color['r'] > 0:
-						dot.color['r'] *= .9992	
-					if dot.color['g'] > 0:
-						dot.color['g'] *= .9992
-					if dot.color['b'] > 0:
-						dot.color['b'] *= .9992
-					dot.radius =  int(round(dot.radius + (Dot.DOT_RADIUS - dot.radius)/(2)))
+    # squares all captured nothing to do anymore
+    if sum([plr.numSquares for plr in PList]) >= totalSquares:
+        print("Here's the Scores:  " + str([plr.playerName + ": " + str(
+            plr.numSquares) + " square(s) captured" for plr in PList]))
+        screen.fill(BGCOLOR)
+        myfont = pygame.font.Font(None, 50)
+        turn = myfont.render(
+            "GAME OVER! P2=" + str(PList[1].numSquares) + "  P1=" + str(PList[0].numSquares), 1, RED)
+        turnRect = turn.get_rect()
+        screen.blit(turn, (0, 10))
+        for rect in rectsToDraw:
+            pygame.draw.polygon(screen, recColorDict[rect], list(rect), 0)
 
-			if distance <= dot.radius:
-				dot.setClrByTuple(LIMEGREEN)
-				dot.radius = int(round(Dot.DOT_RADIUS*1.25))
-				if clickedDot != None and clickedDot != dot:
-					dot.setClrByTuple(RED)
-				if dotclick[0] == False and clickDist <= dot.DOT_RADIUS and dotclick[2] != None and dotclick[2] != dot and ({dot, dotclick[2]} not in connectedDotsSet):
-					dot.setClrByTuple(RED)
-					x1, y1 = dotclick[2].gridPosition[0], dotclick[2].gridPosition[1]
-					x2, y2 = dot.gridPosition[0], dot.gridPosition[1]
-					# print("dot gridPosition is :" + str(dot.gridPosition))
-					# print("clickedDot gridPosition is :" + str(dotclick[2].gridPosition))
-					# someone connected a line!
-					if ((x1==x2+1 or x1==x2-1) and y1==y2) or ((y1==y2+1 or y1==y2-1) and x1==x2):
-						connectedDotsSet.add( frozenset([dotclick[2], dot]) )
-						currPlayerTurn.capturedSet.add(frozenset([dotclick[2], dot]))
-						turnNum += 1
+        for frozenDotSet in connectedDotsSet:
+            dot1, dot2 = frozenDotSet
+            pygame.draw.line(screen, BLACK, dot1.position, dot2.position, 10)
 
-						for square in squares:
-							c1, c2, c3, c4 = square[0]
-							if (c1 in connectedDotsSet) and (c2 in connectedDotsSet) and (c3 in connectedDotsSet) and (c4 in connectedDotsSet):
-								if tuple(square[1]) not in rectsToDraw:
-									rectsToDraw.add( tuple(square[1]) )
-									recColorDict[tuple(square[1])] = currPlayerTurn.color
-									if re == False:
-										re = True
-									currPlayerTurn.numSquares += 1
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
 
-						if re == True:
-							turnNum -= 1
-							re = False
-						if sum([plr.numSquares for plr in PList]) >= totalSquares:
-							print("GAME OVER. Here's the SCOREs : " + str([plr.playerName + ": " + str(plr.numSquares) + " square(s) captured" for plr in PList]))
+            pygame.display.flip()
 
-						currPlayerTurn = PList[turnNum%numPlayers]
+    else:
+        for dot in dots:
+            if not currPlayerTurn.isAI:
+                distance = math.sqrt((pygame.mouse.get_pos()[
+                                     0] - dot.position[0])**2 + (pygame.mouse.get_pos()[1] - dot.position[1])**2)
+                if dotclick[1][0] > 0:
+                    clickDist = math.sqrt(
+                        (dotclick[1][0] - dot.position[0])**2 + (dotclick[1][1] - dot.position[1])**2)
+                else:
+                    clickDist = float("inf")
 
-			if clickDist <= dot.radius and dotclick[0] == True and pygame.mouse.get_pressed()[0] == 1:
-				dot.setClrByTuple(RED)
-				dot.radius = int(round(Dot.DOT_RADIUS*1.25))
-				pygame.draw.line(screen, BLACK, dot.position, pygame.mouse.get_pos(), 10)
-				clickedDot = dot
-		else:
-			pass #the player is an ai, call the players move funtion or whatever
+                if distance > Dot.DOT_RADIUS:
+                    for key in dot.color:
+                        if dot.color['r'] > 0:
+                            dot.color['r'] *= .9992
+                        if dot.color['g'] > 0:
+                            dot.color['g'] *= .9992
+                        if dot.color['b'] > 0:
+                            dot.color['b'] *= .9992
+                        dot.radius = int(
+                            round(dot.radius + (Dot.DOT_RADIUS - dot.radius) / (2)))
 
-		pygame.draw.circle(screen, dot.getClrTuple(), dot.position, dot.radius)
+                if distance <= dot.radius:
+                    dot.setClrByTuple(LIMEGREEN)
+                    dot.radius = int(round(Dot.DOT_RADIUS * 1.25))
+                    if clickedDot is not None and clickedDot != dot:
+                        dot.setClrByTuple(RED)
+                    if dotclick[0] == False and clickDist <= dot.DOT_RADIUS and dotclick[
+                            2] != None and dotclick[2] != dot and ({dot, dotclick[2]} not in connectedDotsSet):
+                        dot.setClrByTuple(RED)
+                        x1, y1 = dotclick[2].gridPosition[
+                            0], dotclick[2].gridPosition[1]
+                        x2, y2 = dot.gridPosition[0], dot.gridPosition[1]
+                        # print("dot gridPosition is :" + str(dot.gridPosition))
+                        # print("clickedDot gridPosition is :" + str(dotclick[2].gridPosition))
+                        # someone connected a line!
+                        if ((x1 == x2 + 1 or x1 == x2 - 1) and y1 ==
+                                y2) or ((y1 == y2 + 1 or y1 == y2 - 1) and x1 == x2):
+                            connectionToProcess = frozenset([dotclick[2], dot])
+                            connectedDotsSet.add(connectionToProcess)
+                            possibleMoveSet.remove(connectionToProcess)
+                            currPlayerTurn.capturedSet.add(connectionToProcess)
+                            turnNum += 1
 
-	#Weird ass glitch, idk, just handle exception and remove the shitty element from set. YOLO
-	# toRemove = []
-	# print connectedDotsSet
-	
-	# 	except AttributeError:re
-	# 		toRemove.append(dotSet)
-	# for item in toRemove:
-	# 	connectedDotsSet.remove(item)
+                            for square in squares:
+                                c1, c2, c3, c4 = square[0]
+                                if (c1 in connectedDotsSet) and (c2 in connectedDotsSet) and (
+                                        c3 in connectedDotsSet) and (c4 in connectedDotsSet):
+                                    if tuple(square[1]) not in rectsToDraw:
+                                        rectsToDraw.add(tuple(square[1]))
+                                        recColorDict[tuple(square[1])] = currPlayerTurn.color
+                                        currPlayerTurn.numSquares += 1
+                                        turnNum -= 1
 
-	pygame.display.flip()
+                            currPlayerTurn = PList[turnNum % numPlayers]
+
+                if clickDist <= dot.radius and dotclick[0] == True and pygame.mouse.get_pressed()[
+                        0] == 1:
+                    dot.setClrByTuple(RED)
+                    dot.radius = int(round(Dot.DOT_RADIUS * 1.25))
+                    pygame.draw.line(screen, BLACK, dot.position,
+                                     pygame.mouse.get_pos(), 10)
+                    clickedDot = dot
+            else:  # the player is an ai, call the players move funtion or whatever
+                pMove = currPlayerTurn.moveLogic(set(connectedDotsSet), set(possibleMoveSet), squares, set(rectsToDraw))
+
+                if pMove is not None:
+                    connectedDotsSet.add(pMove)
+                    possibleMoveSet.remove(pMove)
+                    currPlayerTurn.capturedSet.add(pMove)
+                    turnNum += 1
+
+                    for square in squares:
+                        c1, c2, c3, c4 = square[0]
+                        if (c1 in connectedDotsSet) and (c2 in connectedDotsSet) and (c3 in connectedDotsSet) and (c4 in connectedDotsSet):
+                            if tuple(square[1]) not in rectsToDraw:
+                                rectsToDraw.add(tuple(square[1]))
+                                recColorDict[tuple(square[1])] = currPlayerTurn.color
+                                currPlayerTurn.numSquares += 1
+                                turnNum -= 1
+
+                    currPlayerTurn = PList[turnNum % numPlayers]
+
+            pygame.draw.circle(screen, dot.getClrTuple(),
+                               dot.position, dot.radius)
+
+    # Weird ass glitch, idk, just handle exception and remove the shitty element from set. YOLO
+    # toRemove = []
+    # print connectedDotsSet
+
+    # 	except AttributeError:re
+    # 		toRemove.append(dotSet)
+    # for item in toRemove:
+    # 	connectedDotsSet.remove(item)
+
+    pygame.display.flip()
--- playerLogic.py
+++ playerLogic.py
@@ -1,4 +1,8 @@
+import operator
+
+
 class Player:
+
 	def __init__(self, pName, clr, isComputer=False):
 		self.capturedSet = set()
 		self.playerName = pName
@@ -6,8 +10,92 @@ class Player:
 		self.color = clr
 		self.numSquares = 0
 
-	#move should return which two dots the player wants
-	#to connect based on the current board layout.
-	def moveLogic(self, currBoardLayout):
-		#should return null if real player and use the mouse to determine move
-		return None
+	# move should return which two dots the computer wants
+	# to connect based on the current board layout. The value of a square for
+	# the opponent is -1 and the value of a square for the computer is +1 so
+	# the comp will try to maximize its minimized score. This creates a search
+	# tree of solutions and we will brute force this search tree to the max
+	# depth. Again, probably a shitty thing to do but gotta make it actually
+	# work beforeadding optimizations..... wow this is gonna be hella
+	# exponential. Will look up how to optimize the search tree later.
+
+	def moveLogic(self, connectedDotsSet, possConnections, squares, currentRects):
+		solution = self.recursiveBruteForce(connectedDotsSet, possConnections, squares,currentRects,True, 0, float("-inf"), float("inf"))
+		if solution is not None:
+			return solution[0]
+
+	# for arbitrary players, we can use a tuple of utlity values and index based on player
+	def recursiveBruteForce(self, connectedDotsSet, possConnections, squares, rectsToDraw,MaxTurnLoc, currentMaxxerUtility, alpha, beta):
+		if len(possConnections) == 1:
+			for connection in possConnections:
+				MaxTurn = MaxTurnLoc
+				utility_cpy = currentMaxxerUtility
+				cpy_connectedDotsSet = set(connectedDotsSet)
+				cpy_connectedDotsSet.add(connection)
+				#check if we make a square with this hypothetical move, if so increase utility of this path
+				for square in squares:
+					c1, c2, c3, c4 = square[0]
+					if (c1 in cpy_connectedDotsSet) and (c2 in cpy_connectedDotsSet) and (c3 in cpy_connectedDotsSet) and (c4 in cpy_connectedDotsSet):
+						if tuple(square[1]) not in rectsToDraw:
+							if MaxTurn:
+								utility_cpy += 1
+							else:
+								utility_cpy -= 1
+							
+				return (connection, utility_cpy)
+
+		elif len(possConnections) > 1:
+			connDict = dict()
+			for connection in possConnections:
+				repeatTurn = False
+				MaxTurn = MaxTurnLoc
+				utility_cpy = currentMaxxerUtility
+				cpy_connectedDotsSet = set(connectedDotsSet)
+				cpy_connectedDotsSet.add(connection)
+
+				cpy_possConnections = set(possConnections)
+				cpy_possConnections.remove(connection)
+
+				rectsToDraw_cpy = set(rectsToDraw)
+
+				for square in squares:
+					c1, c2, c3, c4 = square[0]
+					if (c1 in cpy_connectedDotsSet) and (c2 in cpy_connectedDotsSet) and (
+							c3 in cpy_connectedDotsSet) and (c4 in cpy_connectedDotsSet):
+						if tuple(square[1]) not in rectsToDraw_cpy:
+							if MaxTurn:
+								utility_cpy += 1
+							else:
+								utility_cpy -= 1
+							if not repeatTurn:
+								repeatTurn = True
+							rectsToDraw_cpy.add(tuple(square[1]))
+				
+				if repeatTurn:
+					MaxTurn = not MaxTurn
+
+				recurTup = self.recursiveBruteForce(cpy_connectedDotsSet, cpy_possConnections, squares, rectsToDraw_cpy, utility_cpy, not MaxTurn, alpha, beta)
+				connDict[connection] = recurTup[1]				
+				# if MaxTurn:
+				# 	if recurTup[1] > alpha:
+				# 		alpha = recurTup[1]
+				# 	if beta <= alpha:
+				# 		break
+				# else:
+				# 	if recurTup[1] < beta:
+				# 		beta = recurTup[1]
+				# 		connToReturn = connection
+				# 	if beta <= alpha:
+				# 		break
+
+			if MaxTurn:
+				return max(connDict.iteritems(), key=operator.itemgetter(1))
+			else:
+				return min(connDict.iteritems(), key=operator.itemgetter(1))
+
+		else:
+			return None
+
+	def minner(self, possConnections):
+		if len(possConnections == 1):
+			return (possConnections.pop(), -1)
